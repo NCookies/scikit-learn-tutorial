1ba665c92ab036bd2e6ce096c54c1a8b50fa55a7
NCookies <swstar21c@gmail.com>
2017-02-17 15:59:17 +0900

    add plot_stock_market.py but cannt understand about visualization code

--- 2. Sciencific Data Processing/5. Pulling It Together/plot_stock_market.py
+++ 2. Sciencific Data Processing/5. Pulling It Together/plot_stock_market.py
@@ -79,6 +79,7 @@ from sklearn import cluster, covariance, manifold
 ###############################################################################
 # Retrieve the data from Internet
 
+
 # Choose a time period reasonably calm (not too long ago so that we get
 # high-tech firms, and before the 2008 crash)
 d1 = datetime.datetime(2003, 1, 1)
